9e0572db79dbf95e37670946f9345085684c9767
keatonwilson <keatonwilson@me.com>
2018-08-31 10:47:54 -0700

    final changes.

--- scripts_and_lessons/train_and_test.R
+++ scripts_and_lessons/train_and_test.R
@@ -5,8 +5,8 @@ library(tidyverse)
 wine = read_csv("./data/wine.csv")
 split = createDataPartition(wine$varietal, p = 0.85, list = FALSE)
 
-wine_train = wine[split,]
-wine_test = wine[-split,]
+wine_test = wine[sample(nrow(wine), 10), ]
+
 
 write_csv(x = wine_train, path = "./data/wine_train.csv")
 write_csv(x = wine_test, path = "./data/wine_test.csv")
