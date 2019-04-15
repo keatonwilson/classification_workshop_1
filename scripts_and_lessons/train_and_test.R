35dfd6d5f889fb74bb577740b7a3870243cf3a9f
keatonwilson <keatonwilson@me.com>
2019-04-15 10:01:59 -0700

    Updating

--- scripts_and_lessons/train_and_test.R
+++ scripts_and_lessons/train_and_test.R
@@ -5,6 +5,7 @@ library(tidyverse)
 wine = read_csv("./data/wine.csv")
 wine$id = rownames(wine)
 
+set.seed(0)
 wine_test = wine[sample(nrow(wine), 10), ]
 
 #anti join
@@ -20,4 +21,11 @@ wine_test = wine_test %>%
 write_csv(x = wine_train, path = "./data/wine_train.csv")
 write_csv(x = wine_test, path = "./data/wine_test.csv")
 
+wine_test = read_csv(file = "./data/wine_test.csv")
 
+
+wine_final = wine %>%
+  mutate(id = as.numeric(id)) %>%
+  inner_join(wine_test)
+
+write_csv(x = wine_final, path = "./data/wine_final.csv")
